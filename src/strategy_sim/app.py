f2bd4cd3fdfbfe6f8464b903eb14b3d56a85eb47
dgrisham <dgrisham@mines.edu>
2018-10-15 16:56:21 -0600

    output formatted for plots, but plots look bad

--- src/strategy_sim/app.py
+++ src/strategy_sim/app.py
@@ -1,107 +1,151 @@
 #!/usr/bin/env python3
-# PYTHON_ARGCOMPLETE_OK
 # -*- coding: utf-8 -*-
 
-import sys
 import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from numpy import exp, tanh
 
 # local imports
-from sim import run, initialLedgers
-
-# Main
-# ----
+from sim import run
+from plot import plot, mkPlotConfig
 
 
-def main(argv):
+def app():
     # dictionary of reciprocation functions
     rfs = {
-        'linear': lambda x: x,
-        'sigmoid': lambda x: 1 / (1 + exp(2-x)),
-        'tanh': lambda x: tanh(x),
+        "identity": lambda x: x,
+        "sigmoid": lambda x: 1 / (1 + exp(2 - x)),
+        "tanh": lambda x: tanh(x),
     }
-
-    cli = argparse.ArgumentParser()
-    cli.add_argument(
-        '--data',
-        type=int,
-        help="how much data each user should send to each of their peers"
-    )
-    dprArgs = cli.add_mutually_exclusive_group()
-    dprArgs.add_argument(
-        '--dpr',
-        nargs="*",
-        # action='append',
-        type=int,
-    )
-    dprArgs.add_argument(
-        '--dpr-const',
-        type=int,
-    )
-    cli.add_argument(
-        '-u',
-        '--upload-rates',
-        nargs="*",
-        # action='append',
-        type=int,
-    )
-    cli.add_argument(
-        '-f',
-        '--reciprocation-function',
-        choices=rfs.keys(),
-    )
-    cli.add_argument(
-        '-i',
-        '--initial-reputation',
-        choices=['ones', 'split', 'proportional'],
-    )
-    cli.add_argument(
-        '--save-history',
-        action='store_true',
-        default=False,
-    )
-    cli.add_argument(
-        '--save-plot',
-        action='store_true',
-        default=False,
-    )
-    cli.add_argument(
-        '--no-show-plot',
-        action='store_true',
-        default=False,
-    )
-    cli.add_argument(
-        '-o',
-        '--output',
-        default='',
-    )
-    if len(argv) == 0:
-        cli.print_usage()
-        sys.exit()
-    args = cli.parse_args(argv)
-
+    args = cli(rfs.keys())
     function = args.reciprocation_function
     upload_rates = args.upload_rates
-    rep = args.initial_reputation
+    initial_rep = args.initial_reputation
     data = args.data
-    dpr = args.dpr if args.dpr else [args.dpr_const]*len(upload_rates)
+    dpr = args.dpr if args.dpr else [args.dpr_const] * len(upload_rates)
     outfile = args.output
     save_history = args.save_history
     save_plot = args.save_plot
     show_plot = not args.no_show_plot
 
     if not outfile:
-        outfile = f'{function}' \
-            f'-{rep}' \
-            f'-{data}' \
-            f'-{"_".join(str(d) for d in dpr)}' \
+        outfile = (
+            f"{function}"
+            f"-{initial_rep}"
+            f"-{data}"
+            f'-{"_".join(str(d) for d in dpr)}'
             f'-{"_".join(str(r) for r in upload_rates)}'
+        )
 
-    ledgers = initialLedgers(rep, upload_rates)
-    return run(data, dpr, rfs[function], upload_rates, ledgers, outfile,
-               save_history, save_plot, show_plot)
+    history = run(
+        data,
+        dpr,
+        rfs[function],
+        upload_rates,
+        initial_rep,
+        outfile if save_history else "",
+    )
+
+    if save_plot or show_plot:
+        params = pd.DataFrame(
+            index=history.index.get_level_values(1).unique()
+        ).rename_axis("id")
+        params["round_burst"] = str(dpr)
+        params["strategy"] = function
+        params["upload_bandwidth"] = list(map(str, upload_rates))
+
+        ledgers = pd.DataFrame(
+            index=history.reorder_levels([1, 2, 0]).index.rename(
+                ["id", "peer", "time"]
+            ),
+            columns=["recv", "sent", "value"],
+        )
+        ledgers["sent"] = history["send"].cumsum().values
+        for idx, ledger in ledgers.groupby(level=[1, 0, 2]):
+            ledgers.loc[idx, 'recv'] = ledger.iloc[0]['sent']
+        ledgers["value"] = ledgers["recv"] / (ledgers["sent"] + 1)
+
+        ti, tf = ledgers.index.get_level_values("time")[[0, -1]]
+        cfg = mkPlotConfig(ledgers, (ti, tf), params, "all")
+        # TODO: import import plot function from bitswap-tests
+        plot(ledgers, (ti, tf), cfg)
+        plt.show()
+        plt.close()
+
+    return history, params, ledgers
 
 
-if __name__ == '__main__':
-    h = main(sys.argv[1:])
+def cli(rfs):
+    """
+    Process CLI args.
+
+    Inputs:
+        -   rfs ([str]): the list of reciprocation functions that should be accepted as
+            valid input
+    Returns:
+        Parsed arguments.
+    """
+    cli = argparse.ArgumentParser()
+    # fmt: off
+    cli.add_argument(
+        "--data",
+        type=int,
+        help="how much data each user should send to each of their peers",
+        required=True,
+    )
+    dprArgs = cli.add_mutually_exclusive_group(required=True)
+    dprArgs.add_argument(
+        "--dpr",
+        nargs="*",
+        type=int
+    )
+    dprArgs.add_argument(
+        "--dpr-const",
+        type=int,
+    )
+    cli.add_argument(
+        "-u",
+        "--upload-rates",
+        nargs="*",
+        type=int,
+        required=True
+    )
+    cli.add_argument(
+        "-f",
+        "--reciprocation-function",
+        choices=rfs,
+        default="identity"
+    )
+    cli.add_argument(
+        "-i",
+        "--initial-reputation",
+        choices=["ones", "split", "proportional"],
+        default="ones",
+    )
+    cli.add_argument(
+        "--save-history",
+        action="store_true",
+        default=False
+    )
+    cli.add_argument(
+        "--save-plot",
+        action="store_true",
+        default=False
+    )
+    cli.add_argument(
+        "--no-show-plot",
+        action="store_true",
+        default=False
+    )
+    cli.add_argument(
+        "-o",
+        "--output",
+        default=""
+    )
+    # fmt: on
+    return cli.parse_args()
+
+
+h, p, l = app()
--- src/strategy_sim/sim.py
+++ src/strategy_sim/sim.py
@@ -4,7 +4,6 @@
 import sys
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 from math import ceil
 from copy import deepcopy
@@ -12,51 +11,38 @@ from itertools import product
 from collections import defaultdict
 from ledger import Ledger
 
-plt.style.use('ggplot')
-
 __author__ = "David Grisham"
 __copyright__ = "David Grisham"
 __license__ = "mit"
 
 
-def run(data, dpr, rf, upload_rates, initial_ledgers, outfile, save_history,
-        save_plot, show_plot):
-    _, history = propagateN(data, dpr, rf, upload_rates, initial_ledgers)
-    if history is None:
-        return
-    if save_history:
-        history.to_csv('results/{}.csv'.format(outfile))
-    if save_plot or show_plot:
-        plot(history, save_plot, show_plot, outfile)
-    return history
-
-
-def propagateN(data, data_per_round, rf, upload_rates, ledgers):
+def run(data, data_per_round, rf, upload_rates, initial_rep, outfile=""):
+    try:
+        ledgers = initialLedgers(initial_rep, upload_rates)
+    except Exception as e:
+        raise prependErr("initializing ledgers", e)
+    # data_rem[user, peer] gives the amount of data user stil has to send to
+    # peer
     data_rem = {
-        sender: {receiver: data for receiver in ledgers[sender]}
-        for sender in ledgers
+        sender: {receiver: data for receiver in ledgers[sender]} for sender in ledgers
     }
-
-    # t_tot is the number of iterations it will take for all peers to finish
-    t_tot = max(ceil(data/dpr) * ceil(dpr/urate)
-                for dpr, urate in zip(data_per_round, upload_rates)) + 1
 
     # initialize history
     history = pd.DataFrame(
-        index=pd.MultiIndex.from_tuples((
-            (i, x, y) for i, (x, y) in
-            product(range(t_tot+1), product(ledgers.keys(), repeat=2))
-            if x != y
-        ), names=['t', 'i', 'j']), columns=['send', 'debt_ratio'])
+        index=pd.MultiIndex(levels=[[]] * 3, labels=[[]] * 3, names=["t", "i", "j"]),
+        columns=["send", "debt_ratio"],
+    )
     for i, ledgers_i in ledgers.items():
         for j in ledgers_i.keys():
             history.loc[0, i, j] = [0, ledgers[i][j].debtRatio()]
 
-    allocations = {i: {j: 0 for j in ledgers_i.keys()}
-                   for i, ledgers_i in ledgers.items()}
+    allocations = {
+        i: {j: 0 for j in ledgers_i.keys()} for i, ledgers_i in ledgers.items()
+    }
     t = 1
-    active_users = [i for i, di in data_rem.items()
-                    if not all(np.isclose(list(di.values()), 0))]
+    active_users = [
+        i for i, di in data_rem.items() if not all(np.isclose(list(di.values()), 0))
+    ]
     while len(active_users) > 0:
         for sender in active_users:
             upload = upload_rates[sender]
@@ -66,8 +52,7 @@ def propagateN(data, data_per_round, rf, upload_rates, ledgers):
             while not done:
                 for receiver, allocation in allocations[sender].items():
                     # get amount to send to receiver
-                    send = min(
-                        [allocation, upload, data_rem[sender][receiver]])
+                    send = min([allocation, upload, data_rem[sender][receiver]])
                     allocations[sender][receiver] -= send
                     data_rem[sender][receiver] -= send
                     upload -= send
@@ -83,66 +68,77 @@ def propagateN(data, data_per_round, rf, upload_rates, ledgers):
                     done = True
                 elif np.isclose(upload, 0):
                     allocations[sender] = calculateAllocations(
-                        rf, data_per_round[sender], ledgers[sender],
-                        data_rem[sender])
+                        rf, data_per_round[sender], ledgers[sender], data_rem[sender]
+                    )
                     done = True
                 else:
                     allocations[sender] = calculateAllocations(
-                        rf, data_per_round[sender], ledgers[sender],
-                        data_rem[sender])
+                        rf, data_per_round[sender], ledgers[sender], data_rem[sender]
+                    )
 
             # update history with amount sent to each peer
             for receiver, amt in sent.items():
-                history.loc[t, sender, receiver] = \
-                    [amt, ledgers[sender][receiver].debtRatio()]
+                history.loc[t, sender, receiver] = [
+                    amt,
+                    ledgers[sender][receiver].debtRatio(),
+                ]
         t += 1
 
-    return ledgers, history
+    if outfile:
+        history.to_csv("results/{}.csv".format(outfile))
+    return history
 
 
 def calculateAllocations(rf, resource, ledgers, data):
-    weights = {p: rf(l.debtRatio()) for p, l in ledgers.items()
-               if not np.isclose(data[p], 0)}
+    weights = {
+        p: rf(l.debtRatio()) for p, l in ledgers.items() if not np.isclose(data[p], 0)
+    }
     return calculateAllocationsFromWeights(resource, weights)
 
 
 def calculateAllocationsFromWeights(resource, weights):
     total_weight = sum(weight for weight in weights.values())
-    return {p: np.round(resource * weight / total_weight, 1)
-            for p, weight in weights.items()}
+    return {
+        p: np.round(resource * weight / total_weight, 1)
+        for p, weight in weights.items()
+    }
 
 
 def initialLedgers(rep_type, resources, c=0):
-    if rep_type == 'none':
+    if rep_type == "none":
         return defaultdict(lambda: {})
 
-    if rep_type == 'constant':
-        return defaultdict(lambda: {},
-                           {i: {j: Ledger(c, c) for j in range(len(resources))
-                                if j != i}
-                            for i in range(len(resources))
-                            })
+    if rep_type == "constant":
+        return defaultdict(
+            lambda: {},
+            {
+                i: {j: Ledger(c, c) for j in range(len(resources)) if j != i}
+                for i in range(len(resources))
+            },
+        )
 
-    if rep_type == 'ones':
-        return initialLedgers('constant', resources, c=1)
+    if rep_type == "ones":
+        return initialLedgers("constant", resources, c=1)
 
-    if rep_type == 'split':
+    if rep_type == "split":
         ledgers = defaultdict(lambda: {})
         for i, resource_i in enumerate(resources):
             for j, resource_j in enumerate(resources):
                 if j != i:
                     num_partners = len(resources) - 1
                     ledgers[i][j] = Ledger(
-                        resource_j / num_partners, resource_i / num_partners)
+                        resource_j / num_partners, resource_i / num_partners
+                    )
         return ledgers
 
-    if rep_type == 'proportional':
-        ledgers = initialLedgers('constant', resources, c=0)
+    if rep_type == "proportional":
+        ledgers = initialLedgers("constant", resources, c=0)
         reputations = defaultdict(lambda: {})
         for i in range(len(resources)):
             resource = resources[i]
-            weights = {j: resource_j for j,
-                       resource_j in enumerate(resources) if j != i}
+            weights = {
+                j: resource_j for j, resource_j in enumerate(resources) if j != i
+            }
             reputations[i] = calculateAllocationsFromWeights(resource, weights)
 
             initial_ledgers = deepcopy(ledgers)
@@ -152,72 +148,8 @@ def initialLedgers(rep_type, resources, c=0):
                     initial_ledgers[receiver][sender].recv_from += allocation
         return initial_ledgers
 
-    print(f"unsupported reputation type: {rep_type}", sys.stderr)
-    return None
+    raise ValueError(f"unsupported reputation type: {rep_type}")
 
 
-def plot(history, save, show, outfile=''):
-    dr_min = history['debt_ratio'].min()
-    dr_max = history['debt_ratio'].max()
-    dr_mean = history['debt_ratio'].mean()
-    fig, axes = plt.subplots(3)
-    figLog, axesLog = plt.subplots(3)
-
-    axes[0].set_prop_cycle('color', ['black', 'magenta'])
-    axes[1].set_prop_cycle('color', ['blue', 'red'])
-    axes[2].set_prop_cycle('color', ['orange', 'green'])
-
-    axesLog[0].set_prop_cycle('color', ['black', 'magenta'])
-    axesLog[1].set_prop_cycle('color', ['blue', 'red'])
-    axesLog[2].set_prop_cycle('color', ['orange', 'green'])
-
-    for (i, j), hij in history.groupby(level=[1, 2]):
-        hij.index = hij.index.droplevel([1, 2])
-        f = 0.25
-        hij.plot(y='debt_ratio', xlim=(0, hij.index.get_level_values(0).max()),
-                 ylim=(dr_min - f * dr_mean, dr_max + f * dr_mean), ax=axes[i],
-                 label=f"Debt ratio of {j} wrt {i}")
-        hij.plot(y='debt_ratio', xlim=(0, hij.index.get_level_values(0).max()),
-                 ylim=(dr_min * 0.5, dr_max * 1.5), logy=True, ax=axesLog[i],
-                 label=f"Debt ratio of {j} wrt {i}")
-
-        legendFont = 'large'
-        axes[i].legend(prop={'size': legendFont})
-        axesLog[i].legend(prop={'size': legendFont})
-
-        title = f"User {i}'s Debt Ratios"
-        axes[i].set_title(title)
-        axesLog[i].set_title(f"{title} (Semi-Log)")
-
-        ylabel = "Debt Ratio"
-        axes[i].set_ylabel(ylabel)
-        axesLog[i].set_ylabel(f"log({ylabel})")
-
-    # pts = outfile.split('-')
-    # print(pts)
-    # title = f"RF: {pts[0].title()}, IR: {pts[1].title()}, Data: {pts[2]}, DPR: [{pts[3].replace('_', ', ')}], UR: [{pts[4].replace('_', ', ')}], {pts[5].replace('_', ' ').title()}"
-    title = outfile
-
-    fig.suptitle(title)
-    axes[0].set_xlabel('')
-    axes[1].set_xlabel('')
-
-    figLog.suptitle(title)
-    axesLog[0].set_xlabel('')
-    axesLog[1].set_xlabel('')
-
-    fig.tight_layout()
-    figLog.tight_layout()
-
-    plt.setp(axes[0].get_xticklabels(), visible=False)
-    plt.setp(axes[1].get_xticklabels(), visible=False)
-    plt.setp(axesLog[0].get_xticklabels(), visible=False)
-    plt.setp(axesLog[1].get_xticklabels(), visible=False)
-
-    if save and outfile:
-        plt.savefig(f"plots-new/{outfile}.pdf")
-    if show:
-        plt.show()
-
-    plt.clf()
-    plt.close()
+def prependErr(msg, e):
+    return type(e)(f"error {msg}: {e}").with_traceback(sys.exc_info()[2])
